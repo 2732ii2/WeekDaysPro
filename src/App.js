557af79d662b21dbd1525ae1d83f0f8d87ccd7f1
2732ii2 <mo.anas123khan@gmail.com>
2024-05-05 17:27:42 +0530

    card Desiging continue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import logo from './logo.svg';
-import timelogo from '../src/Comp/images/timebottle.jpeg';
+import timelogo from '../src/Comp/images/hourglass.png';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Apicall from './Comp/Api/Apicall';
+import ImgComp from './Comp/ImgComp';
 
 function App() {
   const [page,setpage]=useState(0);
@@ -28,8 +29,24 @@ function App() {
           Data.map((e,i)=>{
             return <div key={i} className='SameDivs'>
               <div className='smallSlit'>
-                 </div>
-
+                <ImgComp src={timelogo} style={{width:"15px",height:"15px"}} />
+                <p className='posted'> Posted on {i+1} days ago </p>
+              </div>
+              <div className='mainslit_'>
+             { e?.logoUrl ?<ImgComp src={e?.logoUrl} style={{width:"40px",height:"55px",marginTop:"-15px",border:"1px solid rgba(0,0,0,.1)",borderRadius:"2px"}} />:null}
+               <div className='insideslit'>
+                  <p className='companyName'>{e?.companyName}</p>
+                  <p className='jobRole'>{`${e?.jobRole} Developer`}</p>
+                  <p className='location'>{e?.location}</p>
+               </div>
+              </div>
+              <p className='estimation'>Estimated Salary :{!e?.minJdSalary ?" ₹10": `₹${e?.minJdSalary }`} - {!e?.maxJdSalary ?"- ": `${e?.maxJdSalary }`} LPA </p>
+              <p className='aboutComp'>About Company: </p>
+              <p className='aboutUs'>About us </p>
+              <p className='desc'>{e?.jobDetailsFromCompany}
+              
+              <strong className='view'>View job</strong>
+              </p>
             </div>
           })
         }
---
+++ src/Comp/ImgComp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ImgComp({src,style}) {
+  return (
+    <>
+    <img src={src} style={style} />
+    </>
+  )
+}
