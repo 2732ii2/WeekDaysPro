7d035ac9197fa13becc94ea73b8e6c1e151c52a3
2732ii2 <mo.anas123khan@gmail.com>
2024-05-05 19:58:49 +0530

    updating infiniteScrolling and loader designing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import timelogo from '../src/Comp/images/hourglass.png';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Apicall from './Comp/Api/Apicall';
 import ImgComp from './Comp/ImgComp';
@@ -9,24 +9,86 @@ function App() {
   const [page,setpage]=useState(0);
   const [Data,setData]=useState([]);
   const [count,setcount]=useState(0);
-  console.log(Data,count);
+  const [loading,setloading]=useState(false);
+  const ref=useRef(null);
+  const [parentNode,setparent]=useState();
+  console.log(parentNode);
+  useEffect(() => {
+    console.log("udpated page",page);
+    (async () => {
+      setloading(true);
+      const data = await Apicall(page);
+      setloading(false);
+      setData(prevData => [...prevData, ...data?.jdList]);
+      setcount(data?.totalCount);
+    })()
+  }, [page]); 
+
+  function onScroll(e){
+    console.log("",e.target.clientHeight,e.target.scrollTop,e.target.scrollHeight);
+    var c=0;
+    if((e.target.scrollTop+1356)>=e.target.scrollHeight ){
+      console.log("getted ");
+      c=1
+    }
+    setTimeout(() => {
+      if(c===1){
+        setpage(page+1)
+      }
+    }, 500);
+  }
+  
   useEffect(()=>{
-   (async ()=>{
-    const data= await Apicall(page)
-    // console.log(data);
-    setData(data?.jdList);
-    setcount(data?.totalCount);
-   })()
-  },[page])
+    if(parentNode){
+      parentNode.addEventListener("scroll",onScroll);
+      return ()=>{
+        parentNode.removeEventListener("scroll",onscroll);
+      }
+    }
+
+  },[parentNode,page])
+
+  useEffect(()=>{
+    if(ref.current){
+      setparent(ref.current.parentElement);
+    }
+
+  },[Data])
   return (
     <div className="App">
       <header className="App-header">
         
       </header>
       {
-        Data.length!==0? <div className='Maincomp'>
+        Data?.length!==0? <div  className='Maincomp'>
         {
           Data.map((e,i)=>{
+            if(i==Data?.length-1){
+               return  <div  ref={ref}  key={i} className='SameDivs'>
+                <div className='smallSlit'>
+                  <ImgComp src={timelogo} style={{width:"15px",height:"15px"}} />
+                  <p className='posted'> Posted on {i+1} days ago </p>
+                </div>
+                <div className='mainslit_'>
+              { e?.logoUrl ?<ImgComp src={e?.logoUrl} style={{width:"40px",height:"55px",marginTop:"-15px",border:"1px solid rgba(0,0,0,.1)",borderRadius:"2px"}} />:null}
+                <div className='insideslit'>
+                    <p className='companyName'>{e?.companyName}</p>
+                    <p className='jobRole'>{`${e?.jobRole} Developer`}</p>
+                    <p className='location'>{e?.location}</p>
+                </div>
+                </div>
+                <p className='estimation'>Estimated Salary :{!e?.minJdSalary ?" ₹10": `₹${e?.minJdSalary }`} - {!e?.maxJdSalary ?"- ": `${e?.maxJdSalary }`} LPA </p>
+                <p className='aboutComp'>About Company: </p>
+                <p className='aboutUs'>About us </p>
+                <p className='desc'>{e?.jobDetailsFromCompany}
+                  <strong className='view'>View job</strong>
+                </p>
+                <button className='apply'>Easy Apply</button>
+                <button className='referral'> Unlock referrals asks </button>
+
+              </div>
+            }
+            else
             return <div key={i} className='SameDivs'>
               <div className='smallSlit'>
                 <ImgComp src={timelogo} style={{width:"15px",height:"15px"}} />
@@ -44,14 +106,16 @@ function App() {
               <p className='aboutComp'>About Company: </p>
               <p className='aboutUs'>About us </p>
               <p className='desc'>{e?.jobDetailsFromCompany}
-              
-              <strong className='view'>View job</strong>
+                <strong className='view'>View job</strong>
               </p>
+              <button className='apply'>Easy Apply</button>
+              <button className='referral'> Unlock referrals asks </button>
+
             </div>
           })
         }
-        
-        </div>:<p>loader ....</p>
+        {loading?<div className='loader'></div>:null}
+        </div>:<div className='loader'></div>
       }
     </div>
   );
